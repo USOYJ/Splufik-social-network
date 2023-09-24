ea4d2da776f139586ff69caaea3a6da02ada20b6
Oyin <125850331+USOYJ@users.noreply.github.com>
2023-09-24 10:02:07 -0400

    routers

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,10 +1,12 @@
-// Create a router instance
 const router = require('express').Router();
-// Import user and thought routes
+
+
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
-// Define endpoints for user and thought routes
-router.use('/user',userRoutes);
-router.use('/thought',thoughtRoutes);
-// Export the router
+
+
+router.use('/user', userRoutes);
+router.use('/thought', thoughtRoutes);
+
+
 module.exports = router;
