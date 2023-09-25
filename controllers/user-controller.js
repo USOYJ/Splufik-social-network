810a30191713779f87146673833216b96891c1a0
Oyin <125850331+USOYJ@users.noreply.github.com>
2023-09-24 20:48:58 -0400

    user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,57 +1,60 @@
 const { User } = require('../models');
 
 const UserController = {
-
   getAllUsers: async (req, res) => {
     try {
       const userData = await User.find({});
       res.json(userData);
     } catch (err) {
+      console.error(err);
       res.status(500).json(err);
     }
   },
 
-
   getUserById: async (req, res) => {
     try {
-      const userData = await User.findById(req.params.userId);
+      const userData = await User.findById(req.params.id); 
       if (!userData) {
         return res.status(404).json({ message: 'User not found' });
       }
       res.json(userData);
     } catch (err) {
+      console.error(err);
       res.status(500).json(err);
     }
   },
-
 
   createUser: async (req, res) => {
     try {
       const userData = await User.create(req.body);
       res.status(201).json(userData);
     } catch (err) {
-      console.log(err)
+      console.error(err);
       res.status(500).json(err);
     }
   },
 
-
   updateUserById: async (req, res) => {
     try {
+      console.log('User ID:', req.params.id);
+      console.log('Request Body:', req.body);
+
       const userData = await User.findOneAndUpdate(
         { _id: req.params.id },
         req.body,
         { new: true }
       );
+
       if (!userData) {
         return res.status(404).json({ message: 'User not found' });
       }
+
       res.json(userData);
     } catch (err) {
+      console.error(err);
       res.status(500).json(err);
     }
   },
-
 
   deleteUserById: async (req, res) => {
     try {
@@ -61,15 +64,15 @@ const UserController = {
       }
       res.json({ message: 'User deleted successfully' });
     } catch (err) {
+      console.error(err);
       res.status(500).json(err);
     }
   },
 
-
   addFriend: async (req, res) => {
     try {
       const userData = await User.findOneAndUpdate(
-        { _id: req.params.userId },
+        { _id: req.params.id }, 
         { $addToSet: { friends: req.body.friendId || req.params.friendId } },
         { new: true }
       );
@@ -78,15 +81,15 @@ const UserController = {
       }
       res.json(userData);
     } catch (err) {
+      console.error(err);
       res.status(500).json(err);
     }
   },
 
-
   removeFriend: async (req, res) => {
     try {
       const userData = await User.findOneAndUpdate(
-        { _id: req.params.userId },
+        { _id: req.params.id }, 
         { $pull: { friends: req.params.friendId } },
         { new: true }
       );
@@ -98,16 +101,18 @@ const UserController = {
       const removed = !userData.friends.includes(req.params.friendId);
 
       if (removed) {
-        res.json({ message: 'Friend removed successfully!', userData });
+        res.json({ message: 'Friend removed successfully', userData });
       } else {
         res.json(userData);
       }
     } catch (err) {
+      console.error(err);
       res.status(400).json(err);
     }
   },
 };
 
 module.exports = UserController;
+
 
 
